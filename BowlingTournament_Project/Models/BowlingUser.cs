237f264a3ba492cf7f2a7415781cda14d11a75d1
omowale01 <oobagunwa01@mynbcc.ca>
2025-12-02 12:54:24 -0400

    bowling user added

--- BowlingTournament_Project/Models/BowlingUser.cs
+++ BowlingTournament_Project/Models/BowlingUser.cs
@@ -1,6 +1,11 @@
-﻿namespace BowlingTournament_Project.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace BowlingTournament_Project.Models
 {
     public class BowlingUser
     {
+        public int Id { get; set; }
+        public string UserName { get; set; } = "";
+        public string PasswordHash { get; set; } = "";
+        public bool IsAdmin { get; set; }
     }
 }
